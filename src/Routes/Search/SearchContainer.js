4c6a8826b2748623091afcf954a2c12256956190
clemado1 <clemado1@gmail.com>
2021-02-24 20:29:56 +0900

    6.3, 6/4

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -8,14 +8,25 @@ export default class extends React.Component {
     tvResults: null,
     searchTerm: "",
     error: null,
-    loading: true
+    loading: false
   };
   
-  handleSubmit = () => {
+  handleSubmit = (event) => {
+    event.preventDefault();
     const { searchTerm } = this.state;
     if (searchTerm !== "") {
       this.searchByTerm();
     }
+  }
+
+  updateTerm = (event) => {
+    const {
+      target: {value}
+    } = event;
+
+    this.setState({
+      searchTerm: value
+    });
   }
 
   searchByTerm = async () => {
@@ -33,7 +44,7 @@ export default class extends React.Component {
       });
     } catch (error) {
       this.setState({
-        error: "Can't fine."
+        error: "Can't find."
       });
     } finally {
       this.setState({
@@ -52,6 +63,7 @@ export default class extends React.Component {
         error={error}
         loading={loading}
         handleSubmit={this.handleSubmit}
+        updateTerm={this.updateTerm}
       />
     );
   }
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,9 +1,46 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from "Components/Loader";
+import Section from "Components/Section";
+import Message from "Components/Message";
+
+const Container = styled.div`
+    padding: 0 20px;
+`;
+
+const Form = styled.form``;
+
+const Input = styled.input`
+    all: unset;
+    font-size: 28px;
+    width: 100%;
+`;
 
 const SearchPresenter = ({
-    movieResults, tvResults, searchTerm, error, loading}) => null;
+    movieResults, tvResults, searchTerm, error, loading, handleSubmit, updateTerm }) => (
+    <Container>
+        <Form onSubmit={handleSubmit}>
+            <Input placeholder="Search Kewords" value={searchTerm} onChange={updateTerm}></Input>
+        </Form>
+        {loading ? (<Loader />) : (
+            <>
+                { movieResults && movieResults.length > 0 && (
+                    <Section title="Movie Results">
+                        {movieResults.map(movie => (<span key={movie.id}>{movie.title}</span>))}
+                    </Section>
+                )}
+                { tvResults && tvResults.length > 0 && (
+                    <Section title="TV Show Results">
+                        {tvResults.map(show => (<span key={show.id}>{show.name}</span>))}
+                    </Section>
+                )}
+                {error && <Message color="red" text={error} />}
+                {tvResults && movieResults && tvResults.length === 0 && movieResults.length === 0 && (<Message color="gray" text="Not Found." />)}
+            </>
+        )}
+    </Container>
+);
 
 SearchPresenter.propTypes = {
     movieResults: PropTypes.array,
@@ -11,6 +48,8 @@ SearchPresenter.propTypes = {
     searchTerm: PropTypes.string,
     error: PropTypes.string,
     loading: PropTypes.bool.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
+    updateTerm: PropTypes.func.isRequired,
 }
 
 export default SearchPresenter;
