76c242b73d030d7c3871dec2cae929153856979c
clemado1 <clemado1@gmail.com>
2021-03-10 21:32:19 +0900

    6.8 ~ 6.9 Detail Presenter

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -25,7 +25,7 @@ const TVPresenter = ({ topRated, popular, airingToday, error, loading }) =>
                                 title={show.original_name} 
                                 rating={show.voie_average}
                                 year={show.first_air_date && show.first_air_date.substring(0, 4)}
-                                isMovie={true}
+                                isMovie={false}
                             />
                         ))}
                     </Section>
@@ -40,7 +40,7 @@ const TVPresenter = ({ topRated, popular, airingToday, error, loading }) =>
                                 title={show.original_name} 
                                 rating={show.voie_average}
                                 year={show.first_air_date && show.first_air_date.substring(0, 4)}
-                                isMovie={true}
+                                isMovie={false}
                             />
                         ))}
                     </Section>
@@ -55,7 +55,7 @@ const TVPresenter = ({ topRated, popular, airingToday, error, loading }) =>
                                 title={show.original_name} 
                                 rating={show.voie_average}
                                 year={show.first_air_date && show.first_air_date.substring(0, 4)}
-                                isMovie={true}
+                                isMovie={false}
                             />
                         ))}
                     </Section>
